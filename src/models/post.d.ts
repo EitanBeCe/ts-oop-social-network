f677278bb6714491b1721a25ea49357d6fd2be93
Eitan <eitanleviberger@gmail.com>
2022-08-31 14:03:58 +0300

    comment change

--- src/models/post.d.ts
+++ src/models/post.d.ts
@@ -2,6 +2,6 @@ export interface PostList {
   list: Post[];
 }
 export interface Post {
-  id?: string; // ? - because when user creates a post there is no id, and it then creates automatically on Firebase
+  id?: string;
   text: string;
 }
