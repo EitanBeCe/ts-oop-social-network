4ec1de7396d155aa918f51ebf60c3683b5dd08cf
Eitan <eitanleviberger@gmail.com>
2022-09-08 17:32:51 +0300

    forEach 3 versions done

--- README.md
+++ README.md
@@ -16,12 +16,15 @@ Write a program with TypeScript (version 4.7 and newer), that meets the followin
 - ✅For UI you can make simple html files (also with simple css styles) (without usage of external libraries like bootstrap)
 - (Full task is [here](TASK.pdf))
 
+❗️Also have a look at the end of full task with universal ForEach class with different implementations
+
 ## What else:
 
 ✅Edded DELETE method
 
 ## Scrins
 
+![Universal ForEach (last task)](/src/img/screenshots/screenForEach.png.png?raw=true 'ForEach')
 ![Alt text](/src/img/screenshots/screenAdd.png?raw=true 'AddScreen')
 ![Alt text](/src/img/screenshots/screenFetch.png?raw=true 'Fetch')
 ![Alt text](/src/img/screenshots/screenModel.png?raw=true 'Models')
--- src/helpers/ForEach.ts
+++ src/helpers/ForEach.ts
@@ -1,71 +1,107 @@
 // Side task
 
-type ListModel = { list: InnerListModel };
-type InnerListModel = ItemModel[];
+type ListModel = { list: InnerArrListModel };
+type InnerArrListModel = ItemModel[];
 type ItemModel = { id: number; name: string };
+
+type ObjListModel = { list: InnerObjListModel };
+type InnerObjListModel = { [key: string]: ItemModel };
+
+type InnerListModel = InnerArrListModel | InnerObjListModel;
+
 type Fn<T> = (item: T, key: keyof T) => void;
 
-// type ArrOfObj = {}[];
-
-class ForEachClass<T extends {}, U extends keyof T> {
-  // Broadest version
-  constructor(someList: T[], key: U, fn: Fn<T>) {
-    someList.forEach((item) => fn(item, key));
+// Broadest version for objectInObj and objectInArr
+class ForEachClass<T extends InnerListModel, A extends keyof ItemModel> {
+  constructor(someList: T, key: A, fn: Fn<ItemModel>) {
+    if (Array.isArray(someList)) {
+      someList.forEach((item) => fn(item, key));
+    }
+    if (typeof someList === 'object' && !Array.isArray(someList)) {
+      for (const [_key, value] of Object.entries(someList)) {
+        fn(value, key);
+      }
+    }
   }
-
-  // Broader version
-  // class ForEachClass<T extends {}, U extends keyof T> {
-  //   constructor(someList: T[], key: U, fn: Fn<T>) {
-  //     someList.forEach((item) => fn(item, key));
-  //   }
-
-  // Narrowed version
-  // constructor(someList: InnerListModel, key: keyof ItemModel, fn: Fn<ItemModel>) {
-  //   someList.forEach((item) => fn(item, key));
-  // }
-  // }
 }
+
+// Broad version
+// class ForEachClass<T extends {}, U extends keyof T> {
+//   constructor(someList: T[], key: U, fn: Fn<T>) {
+//     someList.forEach((item) => fn(item, key));
+//   }
+// }
+
+// Narrowed version
+// class ForEachClass<T extends {}, U extends keyof T> {
+//   constructor(someList: InnerArrListModel, key: keyof ItemModel, fn: Fn<ItemModel>) {
+//     someList.forEach((item) => fn(item, key));
+//   }
+// }
 
 // ************** To call ForEachClass without "new" **************
 
-// Broader version
-const ForEach = <T extends {}, U extends keyof T>(someList: T[], key: U, fn: Fn<T>) => {
+// Broadest version
+const ForEach = <T extends InnerListModel, A extends keyof ItemModel>(
+  someList: T,
+  key: A,
+  fn: Fn<ItemModel>
+) => {
   return new ForEachClass(someList, key, fn);
 };
+// Broad version
+// const ForEach = <T extends {}, U extends keyof T>(someList: T[], key: U, fn: Fn<T>) => {
+//   return new ForEachClass(someList, key, fn);
+// };
 // Narrowed version
-//  const ForEach = (someList: InnerListModel, key: keyof ItemModel, fn: Fn<ItemModel>) => {
+//  const ForEach = (someList: InnerArrListModel, key: keyof ItemModel, fn: Fn<ItemModel>) => {
 //   return new ForEachClass(someList, key, fn);
 // };
 
 // ************** Usage **************
 
 export class LetsUseForEach {
-  private object: ListModel = {
-    list: [
-      { id: 1, name: 'First' },
-      { id: 2, name: 'Second' },
-    ],
-  };
-  private object2: ListModel = {
+  // private object: ListModel = {
+  //   list: [
+  //     { id: 1, name: 'First' },
+  //     { id: 2, name: 'Second' },
+  //   ],
+  // };
+  private objectInArr: ListModel = {
     list: [
       { id: 3, name: 'First' },
       { id: 4, name: 'Second' },
     ],
   };
 
+  private objectInObj: ObjListModel = {
+    list: {
+      item1: { id: 1, name: 'First' },
+      item2: { id: 2, name: 'Second' },
+    },
+  };
+
   constructor() {
-    ForEach(this.object.list ?? [], 'id', (item) => {
-      console.log(`Item: ${JSON.stringify(item)}`);
+    ForEach(this.objectInObj.list ?? {}, 'id', (item, key) => {
+      console.log(`Item in Obj: ${JSON.stringify(item)}. Key: ${item[key]}`);
+      // console.log(`Item: ${JSON.stringify(item.name)}. Key value: ${item[key]}`);
     });
+
     // Key value
-    ForEach(this.object2.list ?? [], 'id', (item, key) => {
-      console.log(`Item: ${JSON.stringify(item)}. Key value: ${item[key]}`);
-      console.log(`Item: ${JSON.stringify(item.name)}. Key value: ${item[key]}`);
+    ForEach(this.objectInArr.list ?? [], 'id', (item, key) => {
+      console.log(`Item in Arr: ${JSON.stringify(item)}. Key value: ${item[key]}`);
+      // console.log(`Item in Arr: ${JSON.stringify(item.name)}. Key value: ${item[key]}`);
     });
+
+    // ForEach(this.object.list ?? [], 'id', (item) => {
+    //   console.log(`Item: ${JSON.stringify(item)}`);
+    // });
+
     // Key
-    ForEach(this.object2.list ?? [], 'id', (item, key) => {
-      console.log(`Item: ${JSON.stringify(item)}. Key: ${key}`);
-    });
+    // ForEach(this.object2.list ?? [], 'id', (item, key) => {
+    //   console.log(`Item: ${JSON.stringify(item)}. Key: ${key}`);
+    // });
+
     // Error example
     // ForEach(this.object2.list ?? [], 'title', (item, key) => {
     //   console.log(`Item: ${JSON.stringify(item)}. Key: ${key}`);
